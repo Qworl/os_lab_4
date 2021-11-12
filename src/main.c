48a2142025f1fa176b2e51becc3416e04ad1e5ef
Qworl <83816505+Qworl@users.noreply.github.com>
2021-11-12 22:38:01 +0300

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <sys/mman.h>
 #include <sys/types.h>
 #include <fcntl.h>
 #include "unistd.h"
@@ -19,38 +20,38 @@ void read_filename(string* str, int fd){
 }
 
 int main(){
-	string file_name;
+    char template[] = "/tmp/tmpXXXXXX";
+    int desc = mkstemp(template);
+    if(desc < 0){
+    	perror("Tmp file not created\n");
+		return -6;
+    }
+	ftruncate(desc, sizeof(float));
+    printf("%s\n", template);
+
+    string file_name;
 	str_create(&file_name);
 	read_filename(&file_name, 0); // 0 = STDIN
 	str_push(&file_name, '\0');
+
 	int file = open(str_get_all(&file_name), O_RDONLY);
-	if(file == -1){
+	if(file < 0){
 		perror("File can't be opened");
 		str_destroy(&file_name);
 		return -5;
 	}
-	str_destroy(&file_name);
-	int fd[2];
-	if(pipe(fd) < 0){
-		perror("pipe error");
-		return -2;
-	}
+
 	int pid = fork();
 	if(pid == -1){
 		perror("fork error");
 		return -1;
 	}
 	if(pid == 0){ //child
-		close(fd[0]);
 		if(dup2(file, 0) < 0){
 			perror("Cannot dup fd[0] to stdin");
 			return -5;
 		}
-		if(dup2(fd[1], 1) < 0){
-			perror("Cannot dup fd[1] to stdout");
-			return -6;
-		}
-		if(execl("child", "", NULL) == -1){
+		if(execl("child", "child", template, NULL) == -1){
 			perror("Execl child problem");
 			return -7;
 		}
@@ -60,12 +61,17 @@ int main(){
 		if(WEXITSTATUS(status)){
 			return -1;
 		}
-		float res = 0.0f;
-		read(fd[0], &res, sizeof(float));
+		float* fd = mmap(0, sizeof(float), 
+    				PROT_READ | PROT_WRITE,
+    				MAP_SHARED, desc, 0);
+	    if (fd == MAP_FAILED){
+	    	perror("mmap error\n");
+			return -5;
+	    }
+		float res = fd[0];
 		printf("%f\n", res);
-		//write(1, &res, sizeof(float));
-		close(fd[0]);
-		close(fd[1]);
+		munmap(fd, sizeof(float));
+		close(desc);
 	}
 	return 0;
 }
